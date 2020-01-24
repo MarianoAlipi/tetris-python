62412f45c5ec12e07caf83ff1c0abad41ed17c28
Mariano Alipi <marianoalipi@gmail.com>
2020-01-24 15:14:01 -0600

    Added some tetrimino creation behaviors and testing.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import arcade as Arcade
+import tetrimino
 
 SCREEN_WIDTH = 300
 SCREEN_HEIGHT = 600
@@ -8,14 +9,22 @@ BOTTOM_Y = 0
 LEFT_X = 0
 RIGHT_X = SCREEN_WIDTH
 
+UP_KEYS = [Arcade.key.UP, Arcade.key.W]
+DOWN_KEYS = [Arcade.key.DOWN, Arcade.key.S]
+LEFT_KEYS = [Arcade.key.LEFT, Arcade.key.A]
+RIGHT_KEYS = [Arcade.key.RIGHT, Arcade.key.D]
+
 class Game(Arcade.Window):
     
     """ Init """
     def __init__(self, width, height, title):
         super().__init__(width, height, title)        
     
+        # Player
         self.player = None
         self.player_list = None
+
+        # Normalized delta time
         self.delta = 0
 
         # Keys
@@ -24,12 +33,18 @@ class Game(Arcade.Window):
         self.left_pressed = False
         self.right_pressed = False
         self.space_pressed = False
+        self.r_pressed = False
 
-        Arcade.set_background_color(Arcade.color.WHITE)
+        # Field
+        w, h = 10, 20
+        self.field = [ [False for i in range(w)] for j in range(h) ]
+
+        Arcade.set_background_color(Arcade.color.BLACK)
 
     """ Setup """
     def setup(self):
-        self.player = Arcade.Sprite(filename="char.gif")
+        self.tetr = tetrimino.Tetrimino(tetrimino.Tetrimino.Type.S, 0, 4)
+        self.player = Arcade.Sprite(filename=self.tetr.img)
         self.player.center_x = 150
         self.player.center_y = 450
         self.player.angle = 0
@@ -45,18 +60,23 @@ class Game(Arcade.Window):
         # Make player rotate (testing)
         # self.player.angle -= 360.0 / 120.0 * self.delta
         if self.up_pressed and not self.down_pressed:
-            self.player.change_y = 3
+            self.player.change_y = 4
         elif self.down_pressed and not self.up_pressed:
-            self.player.change_y = -3
+            self.player.change_y = -4
         else:
             self.player.change_y = 0
 
         if self.left_pressed and not self.right_pressed:
-            self.player.change_x = -3
+            self.player.change_x = -4
         elif self.right_pressed and not self.left_pressed:
-            self.player.change_x = 3
+            self.player.change_x = 4
         else:
             self.player.change_x = 0
+
+        # Rotation
+        if self.r_pressed:
+            self.player.angle -= 90
+            self.r_pressed = False
 
         self.player.center_x += self.player.change_x
         self.player.center_y += self.player.change_y
@@ -71,28 +91,32 @@ class Game(Arcade.Window):
     """ || Input manager || """
     """ =================== """
     def on_key_press(self, key, modifiers):
-        if key == Arcade.key.UP:
+        if key in UP_KEYS:
             self.up_pressed = True
-        elif key == Arcade.key.DOWN:
+        elif key in DOWN_KEYS:
             self.down_pressed = True
-        elif key == Arcade.key.LEFT:
+        elif key in LEFT_KEYS:
             self.left_pressed = True
-        elif key == Arcade.key.RIGHT:
+        elif key in RIGHT_KEYS:
             self.right_pressed = True
         elif key == Arcade.key.SPACE:
             self.space_pressed = True
+        elif key == Arcade.key.R:
+            self.r_pressed = True
 
     def on_key_release(self, key, modifiers):
-        if key == Arcade.key.UP:
+        if key in UP_KEYS:
             self.up_pressed = False
-        elif key == Arcade.key.DOWN:
+        elif key in DOWN_KEYS:
             self.down_pressed = False
-        elif key == Arcade.key.LEFT:
+        elif key in LEFT_KEYS:
             self.left_pressed = False
-        elif key == Arcade.key.RIGHT:
+        elif key in RIGHT_KEYS:
             self.right_pressed = False
         elif key == Arcade.key.SPACE:
             self.space_pressed = False
+        elif key == Arcade.key.R:
+            self.r_pressed = False
 
 """ Main program """
 def main():
--- tetrimino.py
+++ tetrimino.py
@@ -22,13 +22,36 @@ class Tetrimino:
         S = None
         Z = None
 
-    def __init__(self):
-        self._type = None
-        self._img = None
+    def __init__(self, type=None, x=-1, y=-1):
+        self._type = type
+        self.img = "assets/tetriminos/" + type.value + ".png"
         self._rotation = 0
-        # The position of the anchor block (independent of rotation)
+        # Which block is the anchor block (respect to the tetrimino's image).
+        # Starts at 0.
+        anchorBlockX, anchorBlockY = self._determineAnchorBlock(type)
+        self._anchor_block = {'x': anchorBlockX, 'y': anchorBlockY}
+        # The position of the anchor block (independent of rotation) in the field
         # Check which is the anchor block for every type
-        self._anchor_pos = {'x': -1, 'y': -1}
+        self._anchor_pos = {'x': x, 'y': y}
+
+    @classmethod
+    def _determineAnchorBlock(cls, type):
+        if type == None:
+            return -1, -1
+        elif type == cls.Type.I:
+            return 0, 1
+        elif type == cls.Type.O: # Type.O is a special case. It can't be rotated.
+            return 0, 0
+        elif type == cls.Type.T:
+            return 1, 0
+        elif type == cls.Type.J:
+            return 0, 1
+        elif type == cls.Type.L:
+            return 0, 1
+        elif type == cls.Type.S:
+            return 1, 1
+        elif type == cls.Type.Z:
+            return 1, 1
 
     @property
     def type(self):
@@ -54,3 +77,12 @@ class Tetrimino:
     def anchor_pos(self, x, y):
         self._anchor_pos['x'] = x
         self._anchor_pos['y'] = y
+
+    @property
+    def anchor_block(self):
+        return self._anchor_block
+
+    @anchor_block.setter
+    def anchor_block(self, x, y):
+        self._anchor_block['x'] = x
+        self._anchor_block['y'] = y
