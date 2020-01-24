8ad55507edaf05bf201d112b3eb7e1e215825a44
Mariano Alipi <marianoalipi@gmail.com>
2020-01-24 11:40:54 -0600

    Changing structure to implement Arcade library.

--- main.py
+++ main.py
@@ -1,41 +1,52 @@
 import gui
-import time
+import arcade as Arcade
 
-gui.window = gui.create_window(title="Tetris", width=300, height=500)
-gui.canvas = gui.create_canvas()
+SCREEN_WIDTH = 300
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Tetris"
 
+class Game(Arcade.Window):
+    
+    player = None
+
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)        
+
+        Arcade.set_background_color(Arcade.color.WHITE)
+        # Arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Tetris")
+# gui.window = gui.create_window(title="Tetris", width=300, height=500)
+# gui.canvas = gui.create_canvas()
+
+    def setup(self):
+        self.player = Player(, 1)
+
+    """ Render """
+    def on_draw(self):
+        Arcade.start_render()
+
+
+
+    """ Tick """
+    def update(self, delta_time):
+        pass
+
+def main():
+    game = Game(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    game.setup()
+    Arcade.run()
+
+if __name__ == "__main__":
+    main()
+
+"""
 TOP_Y = 0
 BOTTOM_Y = gui.canvas.winfo_reqheight()
 LEFT_X = 0
 RIGHT_X = gui.canvas.winfo_reqwidth()
 
 r = gui.canvas.create_rectangle(50, 20, 150, 80, fill="green")
-  
-i = 1
-
-input("Press ENTER to continue...")
 
 TICK_EVERY = 1 # seconds
 DELTA_LIMIT = TICK_EVERY * 1000000000 # nanoseconds
 
-delta_time = 0.0
-last_time = time.time_ns()
-
-while True:
-
-    current_time = time.time_ns()
-    delta_time = delta_time + (current_time - last_time)
-    last_time = current_time
-
-    # If delta_time reached limit...
-    if (delta_time >= DELTA_LIMIT):
-        # Do tick
-        print("Tick", i)
-        i = i + 1
-
-        # Reset delta_time
-        delta_time = delta_time - DELTA_LIMIT
-
-
-
-input("Press ENTER to exit...")
+"""
