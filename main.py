6b813b5a208d1242bb2c5bf463c65268037dcfe5
Mariano Alipi <marianoalipi@gmail.com>
2020-01-24 11:50:51 -0600

    Cleaned up code and adapted structure to use Arcade library.

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
-import gui
 import arcade as Arcade
 
 SCREEN_WIDTH = 300
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Tetris"
+TOP_Y = SCREEN_HEIGHT
+BOTTOM_Y = 0
+LEFT_X = 0
+RIGHT_X = SCREEN_WIDTH
 
 class Game(Arcade.Window):
     
-    player = None
-
     def __init__(self, width, height, title):
         super().__init__(width, height, title)        
+    
+        self.player = None
+        self.player_list = None
 
         Arcade.set_background_color(Arcade.color.WHITE)
-        # Arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Tetris")
-# gui.window = gui.create_window(title="Tetris", width=300, height=500)
-# gui.canvas = gui.create_canvas()
 
     def setup(self):
-        self.player = Player(, 1)
+        self.player = Arcade.Sprite(filename="char.gif")
+        self.player.center_x = 150
+        self.player.center_y = 450
+
+        self.player_list = Arcade.SpriteList()
+        self.player_list.append(self.player)
+
 
     """ Render """
     def on_draw(self):
         Arcade.start_render()
-
+        self.player_list.draw()
 
 
     """ Tick """
@@ -39,14 +46,6 @@ if __name__ == "__main__":
     main()
 
 """
-TOP_Y = 0
-BOTTOM_Y = gui.canvas.winfo_reqheight()
-LEFT_X = 0
-RIGHT_X = gui.canvas.winfo_reqwidth()
-
-r = gui.canvas.create_rectangle(50, 20, 150, 80, fill="green")
-
 TICK_EVERY = 1 # seconds
 DELTA_LIMIT = TICK_EVERY * 1000000000 # nanoseconds
-
 """
